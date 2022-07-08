13d7ad60b0b0b39d23b202c68680d45402e76d78
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-07-08 17:16:30 +0300

    Component| State

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    name: "Godfrey",
+    age: "25",
+    school: "Moringa",
+    count: 0,
+  }; 
+  Add = () => {
+    this.setState({
+      count: this.state.count +1,
+      school: (this.state.school = "The East African University"),
+    });
+  };
+  Sub = () => {
+    this.setState({
+      count: this.state.count -1,
+    });
+  
+  };
+  render(){
+    return (
+      <>
+      <h1>REACT STATE</h1>
+      <h2>Name: {this.state.name}</h2>
+      <h2>Age: {this.state.age}</h2>
+      <h2>School: {this.state.school}</h2>
+      <h3>Counter Value: {this.state.count}</h3>
+      <button onClick = {this.Add}>Add One</button>
+      <button onClick = {this.Sub}>Sub One</button>
+      </>
+    )
+  }
 }
 
 export default App;
