20b41447bde71ab24fa5fa8c79ae3eebfbd3ad13
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-07-08 17:46:30 +0300

    simple count app

--- src/App.js
+++ src/App.js
@@ -1,37 +1,34 @@
-import React, { Component } from "react";
+import { useState } from "react";
 
-class App extends Component {
-  state = {
-    name: "Godfrey",
-    age: "25",
-    school: "Moringa",
-    count: 0,
-  }; 
-  Add = () => {
-    this.setState({
-      count: this.state.count +1,
-      school: (this.state.school = "The East African University"),
-    });
-  };
-  Sub = () => {
-    this.setState({
-      count: this.state.count -1,
-    });
-  
-  };
-  render(){
+const App = () => {
+  const [count, setCount] = useState(0)
+  // state = {
+  //   name: "Godfrey",
+  //   age: "25",
+  //   school: "Moringa",
+  //   count: 0,
+  // }; 
+   const Add = () => {
+  //   this.setState({
+  //     count: this.state.count +1,
+  //     school: (this.state.school = "The East African University"),
+  //   });
+    setCount(count +1)
+   };
+  const Sub = () => {
+  //   this.setState({
+  //     count: this.state.count -1,
+  //   });
+    setCount(count -1)
+   };
     return (
       <>
       <h1>REACT STATE</h1>
-      <h2>Name: {this.state.name}</h2>
-      <h2>Age: {this.state.age}</h2>
-      <h2>School: {this.state.school}</h2>
-      <h3>Counter Value: {this.state.count}</h3>
-      <button onClick = {this.Add}>Add One</button>
-      <button onClick = {this.Sub}>Sub One</button>
+      <h3>Counter Value: {count}</h3>
+      <button onClick = {Add}>Add One</button>
+      <button onClick = {Sub}>Sub One</button>
       </>
-    )
-  }
-}
+    );
+};
 
 export default App;
